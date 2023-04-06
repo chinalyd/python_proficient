251efb0b3f9c0574ee2d890f670f4d732bf77532
chinalyd <chinalyd0820@sina.cn>
2023-04-06 20:56:19 +0800

    tkinter

---
+++ an.py
@@ -0,0 +1,23 @@
+import tkinter
+root = tkinter.Tk()
+button1 = tkinter.Button(root,
+                        anchor = tkinter.E,
+                        text = 'Button1',
+                        width = 30,
+                        height = 7)
+button1.pack()
+button2 = tkinter.Button(root,
+                        text = 'Button2',
+                        bg = 'blue')
+button2.pack()
+button3 = tkinter.Button(root,
+                         text = 'Button3',
+                         width = 12,
+                         height = 1)
+button3.pack()
+button4 = tkinter.Button(root,
+                         width = 40,
+                         height = 7,
+                         state = tkinter.DISABLED)
+button4.pack()
+root.mainloop()
