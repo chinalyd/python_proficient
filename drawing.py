2719766f577f6fb23228a0c74f1f81f38fcc26f0
chinalyd <chinalyd0820@sina.cn>
2023-04-07 17:18:53 +0800

    tkinter widget'

---
+++ drawing.py
@@ -0,0 +1,82 @@
+import tkinter as Tkinter
+class MyButton:
+    def __inint__(self, root, canvas, label, type):
+        self.root = root
+        self.canvas = canvas
+        self.label = label
+        if type == 0:
+            button = Tkinter.Button(root, text = 'Line', command = self.DrawLine)
+        elif type == 1:
+            button = Tkinter.Button(root, text = 'Arc', command = self.DrawArc)
+        elif type == 2:
+            button = Tkinter.Button(root, text = 'Rec', command = self.DrawRec)
+        else:
+            button = Tkinter.Button(root, text = 'Oval', command = self.DrawOval)
+        button.pack(side = 'left')
+    def DrawLine(self):
+        self.label.text.set('Line')
+        self.canvas.SetStatus(0)
+    def DrawArc(self):
+        self.label.text.set('Arc')
+        self.canvas.SetStatus(1)
+    def DrawRec(self):
+        self.label.text.set('Rec')
+        self.canvas.SetStatus(2)
+    def DrawOval(self):
+        self.label.text.set('Oval')
+        self.canvas.SetStatus(3)
+class MyCanvas:
+    def __init__(self, root):
+        self.status = 0
+        self.draw = 0
+        self.root = root
+        self.canvas = Tkinter.Canvas(root, bg = 'white', width = 600, height = 480)
+        self.canvas.pack()
+        self.canvas.bind('<ButtonRelease-1>',self.Draw)
+        self.canvas.bind('<Button-2>',self.Exit)
+        self.canvas.bind('<Button-3>',self.Del)
+        self.canvas.bind_all('<Delete>',self.Del)
+        self.canvas.bind_all('<KeyPress-d>',self.Del)
+        self.canvas.bind_all('<KeyPress-e>',self.Exit)
+    def Draw(self, event):
+        if self.draw == 0:
+            self.x = event.x
+            self.y = event.y
+            self.draw = 1
+        else:
+            if self.status == 0:
+                self.canvas.create_line(self.x, self.y, event.x, event.y)
+                self.draw = 0
+            elif self.status == 1:
+                self.canvas.create_arc(self.x, self.y, event.x, event.y)
+                self.draw = 0
+            elif self.status == 2:
+                self.canvas.create_rectangle(self.x, self.y, event.x, event.y)
+                self.draw = 0
+            else:
+                self.canvas.create_oval(self.x, self.y, event.x, event.y)
+                self.draw = 0
+    def Del(self, event):
+        items = self.canvas.find_all()
+        for item in items:
+            self.canvas.delete(item)
+    def Exit(self, event):
+        self.root.quit()
+    def SetStatus(self, status):
+        self.status = status
+class MyLabel:
+    def __init__(self, root):
+        self.root = root
+        self.canvas = canvas
+        self.text = Tkinter.StringVar()
+        self.text.set('Draw Line')
+        self.label = Tkinter.Label(root, textvariable = self.text, fg = 'red', width = 50)
+        self.label.pack(side = 'left')
+root = Tkinter.Tk()
+canvas = MyCanvas(root)
+label = MyLabel(root)
+MyButton(root,canvas,label,0)
+MyButton(root,canvas,label,1)
+MyButton(root,canvas,label,2)
+MyButton(root,canvas,label,3)
+root.mainloop()
